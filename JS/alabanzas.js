14b9b24ac37d1439d3500a46665665001d45ce79
asambleacristianaargentina-cmyk <asambleacristiana.argentina@gmail.com>
2026-03-04 13:57:18 -0300

    Update alabanzas.js

--- JS/alabanzas.js
+++ JS/alabanzas.js
@@ -1,29 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+
 const alabanzas = [
-  {
-    id: "alabanza1",
-    titulo: "Te lo debo a Ti"
-  },
-  {
-    id: "alabanza2",
-    titulo: "Necesito de Ti"
-  },
-  {
-    id: "alabanza3",
-    titulo: "Un Milagro"
-  }
+  { id: "alabanza1", titulo: "Te lo debo a Ti" },
+  { id: "alabanza2", titulo: "Necesito de Ti" },
+  { id: "alabanza3", titulo: "Un Milagro" }
 ];
 
-// Render listado
 const lista = document.getElementById("lista-alabanzas");
 
+if (!lista) return;
+
 alabanzas.forEach(alabanza => {
+
   const div = document.createElement("div");
   div.className = "alabanza-item";
   div.textContent = alabanza.titulo;
 
-  div.onclick = () => {
-    window.location.href = `alabanza.html?id=${alabanza.id}`;
-  };
+  div.addEventListener("click", function(){
+    window.location.href = "alabanza.html?id=" + alabanza.id;
+  });
 
   lista.appendChild(div);
+});
+
 });
