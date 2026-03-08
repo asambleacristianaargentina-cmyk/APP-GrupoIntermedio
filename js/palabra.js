636af2edf22cc34b81b49024943bc5cc9113b32a
asambleacristianaargentina-cmyk <asambleacristiana.argentina@gmail.com>
2026-03-08 11:22:27 -0300

    Update palabra.js

--- js/palabra.js
+++ js/palabra.js
@@ -1,3 +1,29 @@
-// Este archivo queda preparado por si en el futuro
-// queremos cargar el versículo o la meditación dinámicamente.
-// Por ahora no hace falta tocar nada.
+const audio = document.getElementById("audio");
+const playBtn = document.getElementById("playBtn");
+const progress = document.getElementById("progress");
+
+playBtn.addEventListener("click", () => {
+
+if(audio.paused){
+audio.play();
+playBtn.textContent = "⏸";
+}else{
+audio.pause();
+playBtn.textContent = "▶";
+}
+
+});
+
+audio.addEventListener("timeupdate", () => {
+
+const percent = (audio.currentTime / audio.duration) * 100;
+progress.value = percent;
+
+});
+
+progress.addEventListener("input", () => {
+
+const time = (progress.value / 100) * audio.duration;
+audio.currentTime = time;
+
+});
